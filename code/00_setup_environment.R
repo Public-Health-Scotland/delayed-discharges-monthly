aa80353ad45e22b5b5e0e5f54fd312551271e150
alicebyers5 <alice.byers@nhs.net>
2020-07-15 14:28:05 +0100

    Create setup environment script

---
+++ code/00_setup_environment.R
@@ -0,0 +1,17 @@
+#########################################################################
+# Name of file - 00_setup_environment.R
+# Data release - Monthly Delayed Discharges publication
+# Original Authors - Alice Byers
+# Orginal Date - July 2020
+#
+# Type - Reproducible Analytical Pipeline
+# Written/run on - RStudio Server
+# Version of R - 3.6.1
+#
+# Description - Sets up environment required for running publication RAP. 
+# This is the only file which should require updating every
+# time the RAP process is run
+#########################################################################
+
+
+### END OF SCRIPT ###
