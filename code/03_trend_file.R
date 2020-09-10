66827faf9bcf0ce1daa36f60bbbca5316edd3ce1
Russell McCreath <r.mccreath@nhs.net>
2020-09-10 18:25:34 +0100

    add specialty descriptions

--- code/03_trend_file.R
+++ code/03_trend_file.R
@@ -23,3 +23,13 @@ walk(list.files(here("functions"), full.names = TRUE), source)
 ### 1 - Import latest Scotland file ----
 
 datafile <- read_rds(here("data", format(start_month, "%Y-%m"), paste0(format(start_month, "%Y-%m"), "_scotland.rds")))
+
+
+### 2 - Data wrangling
+
+## Add specialty description
+# Create lookup file
+lookup_specialty <- read_csv("https://www.opendata.nhs.scot/dataset/688c7ea0-4845-4b03-9df0-4149c72cb7f0/resource/6f2e3da0-b1b5-46cc-ac04-78495daedfa3/download/specialty-reference.csv")
+
+# Add specialty description to datafile
+datafile %<>% left_join(lookup_specialty, by = c("specialty_code" = "Specialty"))
