39abda6c76e70cf09a37797fd733295f65a01c7c
Russell McCreath <r.mccreath@nhs.net>
2020-09-10 17:37:41 +0100

    add initial script structure

---
+++ code/03_trend_file.R
@@ -0,0 +1,16 @@
+#########################################################################
+# Name of file - 03_trend_file.R
+# Data release - Monthly Delayed Discharges publication
+# Original Authors - Russell McCreath
+# Orginal Date - September 2020
+#
+# Type - Reproducible Analytical Pipeline (RAP)
+# Written/run on - RStudio Server
+# Version of R - 3.6.1
+#
+# Description - An update to the trend file with Delayed Discharges 
+# records from most recent publication month.
+#########################################################################
+
+
+### 1 - Import latest Scotland file ----
