cd537f4c9b9e45d59e0972aba26e3c1be9cef0d2
alicebyers5 <alice.byers@phs.scot>
2020-11-09 16:28:15 +0000

    Update lookup filepaths and var names

--- code/00_setup-environment.R
+++ code/00_setup-environment.R
@@ -75,7 +75,7 @@ pc_lookup <- function(){
        "Scottish_Postcode_Directory_2020_2.rds") %>%
     read_rds() %>%
     clean_names() %>%
-    select(pc7, datazone = datazone2011)
+    select(pc7, data_zone = datazone2011)
 }
   
 spec_lookup <- function(){
@@ -89,15 +89,16 @@ location_lookup <- function(){
   glue("{cl_out}/lookups/Unicode/National Reference Files/location.sav") %>%
     read_sav() %>%
     clean_names() %>%
-    select(location, locname)
+    select(location_code = location, 
+           location_name = locname)
 }
   
 hscp_locality_lookup <- function(){
   glue("{cl_out}/lookups/Unicode/Geography/HSCP Locality/",
-       "HSCP Localities_DZ11_Lookup_20191216.rds") %>%
+       "HSCP Localities_DZ11_Lookup_20200825.rds") %>%
     read_rds() %>%
     clean_names() %>%
-    select(datazone = datazone2011,
+    select(data_zone = datazone2011,
            hscp = hscp2019name,
            locality = hscp_locality)
 }
@@ -110,8 +111,8 @@ hb_lookup <- function(){
     clean_names() %>%
     filter(is.na(hb_date_archived)) %>%
     mutate(hb_name = str_replace(hb_name, " and ", " & ")) %>%
-    select(healthboard_code = hb, 
-           healthboard = hb_name)
+    select(health_board_code = hb, 
+           health_board = hb_name)
 }
 
 la_lookup <- function(){
@@ -127,8 +128,8 @@ la_lookup <- function(){
       str_detect(ca_name, " and ") ~ str_replace(ca_name, " and ", " & "),
       TRUE ~ ca_name
     )) %>%
-    select(local_authority_area_code = ca, 
-           local_authority_area = ca_name) %>%
+    select(local_authority_code = ca, 
+           local_authority = ca_name) %>%
     distinct()
 }
 
