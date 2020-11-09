864cf1cc98dd0b67d51637e376aae87c6eb1bab1
alicebyers5 <alice.byers@phs.scot>
2020-11-09 16:28:26 +0000

    Match on lookups

--- code/03_create-trend-file.R
+++ code/03_create-trend-file.R
@@ -38,4 +38,33 @@ trend %<>%
   bind_rows(scotland)
 
 
+### 3 - Match lookup files ----
+
+trend %<>%
+  
+  # Remove existing matched variables
+  select(-c(data_zone, hscp, locality, location_name,
+            health_board_code, local_authority_code)) %>%
+  
+  # Match on new matched variables
+  
+  # Datazone
+  left_join(pc_lookup(), by = c("patient_postcode" = "pc7")) %>%
+  relocate(data_zone, .after = patient_postcode) %>%
+  
+  # HSCP and Locality
+  left_join(hscp_locality_lookup(), by = "data_zone") %>%
+  relocate(hscp, locality, .after = local_authority) %>%
+  
+  # Location
+  left_join(location_lookup(), by = "location_code") %>%
+  relocate(location_name, .after = location_code) %>%
+  
+  # Health Board and Local Authority codes
+  left_join(hb_lookup(), by = "health_board") %>%
+  left_join(la_lookup(), by = "local_authority") %>%
+  relocate(health_board_code, .after = health_board) %>%
+  relocate(local_authority_code, .after = local_authority)
+
+
 ### END OF SCRIPT ###
